9ec12e0c00031aed1bdea3600da3377eec0a9b9c
Unpublished <unpublished@gmx.net>
2019-09-25 13:30:51 +0200

    try BrowserRouter

--- src/sentry/static/sentry/app/main.jsx
+++ src/sentry/static/sentry/app/main.jsx
@@ -1,6 +1,7 @@
 import {hot} from 'react-hot-loader/root'; // This needs to come before react
 import React from 'react';
 import {Router, browserHistory} from 'react-router';
+import {useBasename} from 'history';
 
 import routes from 'app/routes';
 import {loadPreferencesState} from 'app/actionCreators/preferences';
@@ -12,7 +13,7 @@ class Main extends React.Component {
 
   render() {
     return (
-      <Router basename="/sentry/" history={browserHistory}>
+      <Router history={useBasename(() => browserHistory)({basename: '/sentry'})}>
         {routes()}
       </Router>
     );
